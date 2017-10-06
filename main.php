c87d39b9132a70b822a6aa57cb91d9eff3b0bdb3
arttaksin <solo_blackshadow@hotmail.com>
2017-10-06 11:36:53 +0700

    home

--- main.php
+++ main.php
@@ -29,68 +29,9 @@
 
                         <h1 class="uk-article-title">
                         </h1>
-<<<<<<< HEAD
                         <?php include("note_it.php"); ?>
 
                     </article>
-=======
-                        <div class="uk-grid uk-grid-match" data-uk-grid-match="{target:'.uk-panel'}">
-                            <div class="uk-width-medium-1-3">
-                                <div class="uk-panel">Lorem ipsum dolor sit amet.</div>
-                            </div>
-                        </div>
-                        <div class="uk-grid uk-grid-match" data-uk-grid-match="{target:'.uk-panel'}">
-                            <div class="uk-width-medium-1-3">
-                                <div class="uk-panel">Lorem ipsum dolor sit amet.</div>
-                            </div>
-                        </div>
-                         <div class="uk-grid uk-grid-match" data-uk-grid-match="{target:'.uk-panel'}">
-                            <div class="uk-width-medium-1-3">
-                                <div class="uk-panel">Lorem ipsum dolor sit amet.</div>
-                            </div>
-                        </div>
-                         <div class="uk-grid uk-grid-match" data-uk-grid-match="{target:'.uk-panel'}">
-                            <div class="uk-width-medium-1-3">
-                                <div class="uk-panel">Lorem ipsum dolor sit amet.</div>
-                            </div>
-                        </div>
-                         <div class="uk-grid uk-grid-match" data-uk-grid-match="{target:'.uk-panel'}">
-                            <div class="uk-width-medium-1-3">
-                                <div class="uk-panel">Lorem ipsum dolor sit amet.</div>
-                            </div>
-                        </div>
-                         <div class="uk-grid uk-grid-match" data-uk-grid-match="{target:'.uk-panel'}">
-                            <div class="uk-width-medium-1-3">
-                                <div class="uk-panel">Lorem ipsum dolor sit amet.</div>
-                            </div>
-                        </div>
-                         <div class="uk-grid uk-grid-match" data-uk-grid-match="{target:'.uk-panel'}">
-                            <div class="uk-width-medium-1-3">
-                                <div class="uk-panel">Lorem ipsum dolor sit amet.</div>
-                            </div>
-                        </div>
-                         <div class="uk-grid uk-grid-match" data-uk-grid-match="{target:'.uk-panel'}">
-                            <div class="uk-width-medium-1-3">
-                                <div class="uk-panel">Lorem ipsum dolor sit amet.</div>
-                            </div>
-                        </div>
-                         <div class="uk-grid uk-grid-match" data-uk-grid-match="{target:'.uk-panel'}">
-                            <div class="uk-width-medium-1-3">
-                                <div class="uk-panel">Lorem ipsum dolor sit amet.</div>
-                            </div>
-                        </div>
-                        <div class="uk-grid uk-grid-match" data-uk-grid-match="{target:'.uk-panel'}">
-                            <div class="uk-width-medium-1-3">
-                                <div class="uk-panel">Lorem ipsum dolor sit amet.</div>
-                            </div>
-                        </div>
-                  </article>
->>>>>>> 9f8b6554af7657249e7ba16771d03b88248255ee
-         
-
-
-
-
             </div>
 
     </div>
