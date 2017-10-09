e1e1e307387476dcceec0463ee41fe8b75eab686
arttaksin <solo_blackshadow@hotmail.com>
2017-10-09 16:54:04 +0700

    post

--- note_it.php
+++ note_it.php
@@ -1,10 +1,15 @@
-<?php
-    include 'session.php';
-?>
            <form class = "uk-form">
                 <fieldset data-uk0margin>
+                    <div class="uk-form-row">
                     <input type = "text" name = "user" placeholder="ชื่อ">
                     <input type = "text" name= "job" placeholder="แผนก">
                     <input type = "text" name = "location" placeholder="สถานที่">
+                    </div>
+                    <div class="uk-form-row">
+                    <textarea cols="50" rows="10" ></textarea>
+                    </div>
+                    <div class="uk-form-row">
+                         <button class=" uk-button uk-button-success" type="submit" name="Submit">Post</button>
+                    </div>
                 </fieldset>
             </form>
